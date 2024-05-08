291b73eefe4de202449186f49cc6b1a9c062d7c2
lishiwei <lishiwei@piesat.cn>
2024-05-08 17:06:03 +0800

    编辑demo脚本

--- Demo/Demo.cs
+++ Demo/Demo.cs
@@ -4,33 +4,29 @@ using UnityEngine;
 
 public class Demo : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        void Start()
+        LogConfig cfg = new LogConfig
         {
-            LogConfig cfg = new LogConfig
-            {
-                enableLog = true,
-                logPrefix = "",
-                enableTime = true,
-                logSeparate = ">",
-                enableThreadID = true,
-                enableTrace = false,
-                enableSave = true,
-                enableCover = true,
-                savePath = Application.persistentDataPath + "/LogTool/",
-                saveName = "ClientLogTool.txt",
-                loggerEnum = LoggerType.Unity,
-            };
-            LogTool.InitSettings(cfg);
-            LogTool.ColorLog(LogColor.Red, "Color Log:Red.");
-            LogTool.ColorLog(LogColor.Green, "Color Log:Green.");
-            LogTool.ColorLog(LogColor.Blue, "Color Log:Blue.");
-            LogTool.ColorLog(LogColor.Cyan, "Color Log:Cyan.");
-            LogTool.ColorLog(LogColor.Magenta, "Color Log:Magenta.");
-            LogTool.ColorLog(LogColor.Yellow, "Color Log:Yellow.");
-        } 
-    }
+            enableLog = true,
+            logPrefix = "",
+            enableTime = true,
+            logSeparate = ">",
+            enableThreadID = true,
+            enableTrace = false,
+            enableSave = true,
+            enableCover = true,
+            savePath = Application.persistentDataPath + "/LogTool/",
+            saveName = "ClientLogTool.txt",
+            loggerEnum = LoggerType.Unity,
+        };
+        LogTool.InitSettings(cfg);
+        LogTool.ColorLog(LogColor.Red, "Color Log:Red.");
+        LogTool.ColorLog(LogColor.Green, "Color Log:Green.");
+        LogTool.ColorLog(LogColor.Blue, "Color Log:Blue.");
+        LogTool.ColorLog(LogColor.Cyan, "Color Log:Cyan.");
+        LogTool.ColorLog(LogColor.Magenta, "Color Log:Magenta.");
+        LogTool.ColorLog(LogColor.Yellow, "Color Log:Yellow.");
+    } 
     
 }
